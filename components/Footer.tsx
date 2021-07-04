64fc393347ce45cba1eb2c9f3838954b09211314
alex <cuatroalejandro@gmail.com>
2021-07-04 12:00:51 -0400

    add social networks

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,10 @@ export default function Footer() {
     const currentYear = new Date().getFullYear();  
 
     return (
-        <footer>
+        <footer className="mt-10">
+            <div>
+                <img className="w-full max-h-10" src="/svgs/instagram.svg" alt="" />
+            </div>
             <div className="text-center mt-4 text-lg ">
                 <small>&copy; {currentYear} All rights reserved , Neuropsico</small> 
             </div>
