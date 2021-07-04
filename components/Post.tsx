41f93e5ec28a38d924556589b7051cf847d4cead
alex <cuatroalejandro@gmail.com>
2021-07-04 12:01:06 -0400

    update posts styles

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,13 +1,15 @@
 
 export default function Post() {
     return (
-        <div className="flex space-x-2">
-            <div>
-                <h1 className=" font-bold text-sm">
-                    Actividades para estimular la memoria en adultos
-                </h1>          
+        <div className="mt-2">
+            <img className="w-full" src="/images/logo.jpg" alt="" />
+            <div className="flex justify-between px-1 mt-2 text-gray-500 font-medium">
+                <small>02/12/2015</small>
+                <small>4 min </small>
             </div>
-            <img className="h-16" src="/images/logo.jpg" alt="" />
+            <h1 className=" font-bold text-lg mt-2">
+                Actividades para estimular la memoria en adultos
+            </h1>          
         </div>
     )
 }
