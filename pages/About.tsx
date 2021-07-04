d03e726a58cb9fd811fd62278234129c0c99eab6
alex <cuatroalejandro@gmail.com>
2021-07-04 12:01:15 -0400

    add presentation

--- pages/About.tsx
+++ pages/About.tsx
@@ -6,12 +6,19 @@ import Footer from '../components/Footer';
 export default function About() {
     return (
         <div>
-            <Navbar/>
             <main>
-                <div className="p-10">
-                    <h1 className="flex justify-center text-4xl font-bold">About Me</h1>
+                {/* presentation */}
+                <div className="relative px-16 py-40 bg-gradient-to-r from-purple-700 via-pink-600 to-red-500">
+                    <div className="absolute inset-0">
+                        <Navbar/>
+                    </div>
+                    <div className="flex justify-center">
+                        <h1 className="font-sans text-gray-50 text-4xl p-2 font-bold  ">
+                            About me
+                        </h1>
+                    </div>
                 </div>
-                <div className="flex text-justify px-4">
+                <div className="flex text-justify px-4  mt-10">
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias debitis consectetur temporibus animi veniam obcaecati non. Excepturi earum reiciendis repellendus blanditiis quas dolore voluptas! Molestias accusantium reprehenderit maxime atque aut!
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore possimus error facilis deserunt beatae excepturi voluptate earum minima non doloribus. Suscipit sunt magnam eligendi in nobis assumenda, laboriosam sequi iure.
                 </div>
