1ff29139dbfc94bd4bd1edfe90e97d32e49d9f37
alex <cuatroalejandro@gmail.com>
2021-07-04 13:43:46 -0400

    update posts component and lasts components

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,13 +1,16 @@
 
 export default function Post() {
     return (
-        <div className="mt-2">
+        <div className="">
             <img className="w-full" src="/images/logo.jpg" alt="" />
-            <div className="flex justify-between px-1 mt-2 text-gray-500 font-medium">
-                <small>02/12/2015</small>
+            <div 
+                className="flex justify-between px-1 mt-2 text-gray-500 font-medium md:text-lg 
+            ">
+                 <small>02/12/2015</small>
                 <small>4 min </small>
             </div>
-            <h1 className=" font-bold text-lg mt-2">
+            <h1 className=" font-bold text-lg text-gray-900
+             mt-2 md:text-2xl">
                 Actividades para estimular la memoria en adultos
             </h1>          
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,6 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Post from '../components/Post';
 
-
-// TODO 1: develop home presentation 
 // TODO 2: develop or find a good svg after SEO
 // TODO 3: update post and develop posts components
 
@@ -46,12 +44,21 @@ export default function Home() {
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi vitae minus, sunt aliquid minima facere reiciendis nostrum. Deserunt, natus praesentium odit mollitia, enim officia laboriosam, ab quaerat dolores amet est?
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima nostrum, quos harum suscipit eaque sequi provident ducimus non neque maxime, commodi sit mollitia ullam iste corrupti ipsum earum tenetur odit.
                 </div>
-                <img className="my-10 px-2 rounded-xl  shadow-xl" src="/images/logo.jpg" alt="" />
+                <img 
+                    className="my-10 shadow-xl w-full md:max-w-lg md:mx-auto" 
+                    src="/images/logo.jpg" 
+                    alt="" 
+                />
 
                 {/* lasts posts */}
-                <div>
-                    <h1 className="text-2xl font-bold text-gray-900 p-4">Last Posts</h1>
-                    <div className="px-4 space-y-2">
+                <div className="">
+                    <div className="xl:flex xl:justify-center">
+                        <h1 className="text-2xl font-bold text-gray-900 p-4 lg:text-3xl xl:text-4xl">
+                            Lastest from NeuroPsico
+                        </h1>
+                    </div>
+                    <div className="px-4 space-y-2 mt-2 lg:space-y-0 lg:grid lg:grid-cols-2 lg:mx-auto lg:max-w-4xl lg:space-x-4">
+                        <Post/>
                         <Post/>
                         <Post/>
                         <Post/>
