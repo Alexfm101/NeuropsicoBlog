96670ef1d55ea5c27fee6b3d4e9d563abd64be04
alex <cuatroalejandro@gmail.com>
2021-08-27 23:46:57 -0400

    more updates

--- pages/About.tsx
+++ pages/About.tsx
@@ -18,9 +18,11 @@ export default function About() {
                         </h1>
                     </div>
                 </div>
-                <div className="mt-5 flex flex-col text-justify px-4 text-gray-600 lg:w-full lg:mx-auto lg:max-w-4xl xl:max-w-5xl lg:text-lg">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi vitae minus, sunt aliquid minima facere reiciendis nostrum. Deserunt, natus praesentium odit mollitia, enim officia laboriosam, ab quaerat dolores amet est?
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima nostrum, quos harum suscipit eaque sequi provident ducimus non neque maxime, commodi sit mollitia ullam iste corrupti ipsum earum tenetur odit.
+                <div className="mt-10 flex flex-col px-4 md:flex-row lg:w-full lg:mx-auto lg:max-w-4xl xl:max-w-5xl ">
+                    <p className="text-justify text-gray-600 md:p-10 md:font-normal lg:text-lg ">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi vitae minus, sunt aliquid minima facere reiciendis nostrum. Deserunt, natus praesentium odit mollitia, enim officia laboriosam, ab quaerat dolores amet est?
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima nostrum, quos harum suscipit eaque sequi provident ducimus non neque maxime, commodi sit mollitia ullam iste corrupti ipsum earum tenetur odit.
+                    </p>
                     <img className=" mt-5 rounded-2xl shadow-2xl w-60 mx-auto" src="/images/perfil.jpg" alt="" />
                 </div>
             </main>
