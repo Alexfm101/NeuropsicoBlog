e73105e8a65b0e5ab89674a23a482fb636951374
alex <cuatroalejandro@gmail.com>
2021-08-27 23:46:46 -0400

    update about

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,12 +1,12 @@
 
 export default function Post() {
     return (
-        <div className="">
+        <div className="rounded-lg shadow-xl p-5">
             <img className="w-full" src="/images/logo.jpg" alt="" />
             <div 
                 className="flex justify-between px-1 mt-2 text-gray-500 font-medium md:text-lg 
             ">
-                 <small>02/12/2015</small>
+                <small>02/12/2015</small>
                 <small>4 min </small>
             </div>
             <h1 className=" font-bold text-lg text-gray-900
