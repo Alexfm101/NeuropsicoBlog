b54c20b9b101fbb56fff431fa5d2eba6d50b6ae6
alex <cuatroalejandro@gmail.com>
2021-08-29 23:16:40 -0400

    add contact

---
+++ pages/Contact.tsx
@@ -0,0 +1,59 @@
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+export default function Contact() {
+    return (
+        <div>
+            <main>
+                {/* presentation */}
+                <div className="relative px-16 py-40 bg-gradient-to-r from-purple-700 via-pink-600 to-red-500">
+                    <div className="absolute inset-0">
+                        <Navbar/>
+                    </div>
+                    <div className="flex justify-center">
+                        <h1 className="font-sans text-gray-50 text-4xl lg:text-5xl p-2 font-bold  ">
+                            Contact Me
+                        </h1>
+                    </div>
+                </div>
+                <section className="mt-10 md:max-w-xl md:flex md:mx-auto">
+                    <form className="p-4 space-y-5">
+                        <div className="space-y-5 md:flex md:flex-row md:space-y-0 md:space-x-2">
+                            <input
+                                className="border-2 w-full p-2 border-gray-700 rounded-lg focus:outline-none focus:border-red-400 shadow-xl"
+                                type="text"
+                                placeholder="Nombre"
+                                name="email"
+                                id="name"
+                            />
+                            <input
+                                className="border-2 w-full p-2 border-gray-700 rounded-lg focus:outline-none focus:border-red-400 shadow-xl"
+                                type="email"
+                                placeholder="correo"
+                                name="email"
+                                id="email"
+                            />
+                        </div>
+                        <textarea
+                            className="border-2 w-full p-2 border-gray-700 rounded-lg focus:outline-none focus:border-red-400 shadow-2xl"
+                            name=""
+                            id=""
+                            cols={30}
+                            rows={15}
+                            placeholder="En que te puedo ayudar"
+                        />
+                        <div className="flex justify-center">
+                            <button
+                                className=" bg-gray-800 px-6 py-2 rounded-xl text-xl text-white font-Comfortaa font-bold focus:outline-none focus:bg-red-400"
+                                type="submit"
+                            >
+                                Enviar
+                            </button>
+                        </div>
+                    </form>
+                </section>
+            </main>
+            <Footer/>
+        </div>
+    );
+};
