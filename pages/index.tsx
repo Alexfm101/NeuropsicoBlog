1f652bcbf53c078c4f2503c09268cb479a1bee4c
alex <cuatroalejandro@gmail.com>
2021-09-20 23:29:59 -0400

    update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,6 @@ export default function Home({lastArticlesData}) {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
         
-
             <main className="">
                 {/* presentation */}
                 <div 
